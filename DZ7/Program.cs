39ef3cc63539b6a58659a371078ef7fe4b858cd3
ArtemXa <112820278+ArtemXa@users.noreply.github.com>
2022-10-18 17:20:41 +0300

    Update Program.cs

--- DZ7/Program.cs
+++ DZ7/Program.cs
@@ -1,4 +1,4 @@
-﻿//Zadacha47();
+//Zadacha47();
 void Zadacha47()
 {
     Console.WriteLine("Задайте количество строк двумерного массива:");
@@ -7,22 +7,22 @@ void Zadacha47()
     int n = Convert.ToInt32(Console.ReadLine());
     double[,] twoDimArray = new double[m, n];
     Random rnd = new Random();
-    void PrintArray(double[,] matr)
+    void PrintArray(double[,] array)
     {
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
-            { Console.Write($"{matr[i, j]} "); }
+            { Console.Write($"{array[i, j]} "); }
             Console.WriteLine();
         }
     }
 
-    void FillArray(double[,] matr)
+    void FillArray(double[,] array)
     {
         for (int i = 0; i < m; i++)
         {
             for (int j = 0; j < n; j++)
-            { matr[i, j] = Convert.ToDouble(rnd.Next(-100, 100) / 10.0); }
+            { array[i, j] = Convert.ToDouble(rnd.Next(-100, 100) / 10.0); }
         }
     }
     FillArray(twoDimArray);
